0d618aff35bca70e77f7747178af7ac8ad9766af
Ben Carpenter <ben.carpenter@bookrenter.com>
2009-09-23 15:16:34 -0700

    created log dir modified tests to use log dir instead of hardcoded developer's path

--- test/remote/fedex_test.rb
+++ test/remote/fedex_test.rb
@@ -6,7 +6,7 @@ class FedExTest < Test::Unit::TestCase
     @packages  = TestFixtures.packages
     @locations = TestFixtures.locations
     @carrier   = FedEx.new(fixtures(:fedex))
-    FedEx.logger = Logger.new('/Users/james/.active_merchant/fedex.log')
+    FedEx.logger = Logger.new(File.dirname(__FILE__) + '/../../log/fedex.log')
   end
     
   def test_us_to_canada
